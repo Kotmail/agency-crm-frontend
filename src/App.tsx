e39f3ce9848dd37bdf55edfb8e4e6cb4d6165877
Kotmail <ya.kotmail@yandex.ru>
2024-02-05 20:48:57 +0300

    Small fix.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { CircularProgress, ScopedCssBaseline } from "@mui/material"
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom"
 import { Dashboard } from "./pages/dashboard"
 import { useVerifyUserQuery } from "./redux/api/authApi"
-import { FC, useEffect } from "react"
+import { FC, useLayoutEffect } from "react"
 import { UsersPage } from "./pages/dashboard/UsersPage"
 
 const App: FC = () => {
@@ -12,7 +12,7 @@ const App: FC = () => {
   const navigate = useNavigate()
   const { isLoading, isSuccess } = useVerifyUserQuery()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (isSuccess && location.pathname === '/') {
       navigate('/dashboard')
     }
